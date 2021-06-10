f614a702cf2b6e3f6d90bff794c49f0630ddf9c8
Douglas de Freitas Amorim <douglas.freitas@polichat.com.br>
2021-06-10 10:45:39 -0300

    Adição de tabela plugins e tokens no script polics.sql

--- docs/polics.sql
+++ docs/polics.sql
@@ -83,6 +83,35 @@ INSERT into usuarios(nome, email, senha, bloqueado, papel_id, empresa_id)
 VALUES ('Administrador Geral', 'admin@polics.com.br', '123456', false, 1, 1);
 
 
+CREATE TABLE plugins(
+	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	nome varchar(255) NOT NULL,
+	apelido varchar(255),
+	titulo varchar(255) NOT NULL,
+	descricao TEXT NOT NULL
+);
+
+INSERT into plugins(nome, titulo, descricao) VALUES ('Piperun', 'Piperun', 'Este plugin oferece um conjunto chamado 
+	de Atividades onde existe um acordeon com a contagem de atividades e o nome de um funil especifico, sendo possível 
+	abrir o mesmo e visualizar uma planilha com os campos (Atividade,Titulo,Notas,Criação,Dono,Etapa,Funil,Status)');
+INSERT into plugins(nome, titulo, descricao) VALUES ('Polichat', 'Polichat', 'Este plugin oferece um conjunto chamado de 
+	Contatos onde o mesmo traz uma tabela contendo os campos (Id,Nome,Número,Empresa,Conversa)');
+INSERT into plugins(nome, titulo, descricao) VALUES ('Superlógica', 'Superlógica', 'Pagamentos');
+INSERT into plugins(nome, titulo, descricao) VALUES ('Datawarehouse', 'Datawarehouse', 'Banco pessoal');
+
+CREATE TABLE tokens(
+	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    empresa_id int NOT NULL,
+    id_plugin int NOT NULL,
+	FOREIGN KEY (empresa_id) REFERENCES empresas(id),
+	FOREIGN KEY (id_plugin) REFERENCES plugins(id),
+	primeiro_token TEXT NOT NULL,
+	segundo_token TEXT
+);
+
+INSERT into tokens(empresa_id, id_plugin, primeiro_token) VALUES (1, 1, 'OWFlN');
+INSERT into tokens(empresa_id, id_plugin, primeiro_token) VALUES (1, 2, 'R1l6W');
+INSERT into tokens(empresa_id, id_plugin, primeiro_token, segundo_token) VALUES (1, 3, 'Zq5b', 'tf8nfi');
 
 
 
@@ -93,4 +122,6 @@ DROP TABLE usuarios;
 DROP TABLE papeis_permissoes;
 DROP TABLE papeis;
 DROP TABLE permissoes;
+DROP TABLE tokens;
+DROP TABLE plugins;
 DROP TABLE empresas;
